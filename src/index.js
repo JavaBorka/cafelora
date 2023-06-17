8ee3f55f4a1494b900df129f5958e1dfd794bc1e
JavaBorka <borka34@gmail.com>
2023-06-18 00:34:31 +0200

    upravený index.html a vytvorenie komponent

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './style.css';
 
+import { Header } from './components/Header';
+import { Banner } from './components/Banner';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+import { Gallery } from './components/Gallery';
+import { Menu } from './components/Menu';
+
 console.log('funguju!');
+
+const pageElm = document.createElement('div')
+pageElm.classList.add('page')
+
+const mainElm = document.createElement('main')
+mainElm.append(Banner(), Menu(), Gallery(), Contact())
+
+pageElm.append(Header(), mainElm, Footer())
+
+const appElm = document.querySelector('#app')
+appElm.append(pageElm)
+
